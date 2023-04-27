4478a8ff8867ed2abf8cf0c9b3b6d46d91827054
kotbarmaglot <allonewage@gmail.com>
2023-04-27 10:19:57 +0300

    init task13

--- hexlet_flask/example.py
+++ hexlet_flask/example.py
@@ -12,24 +12,35 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/users/')
-def show_users():
-    term = request.args.get('term', default=None)
-    out_list = []
-    in_list = []
+@app.route('/users')
+def users_post():
+    user = {'name': '',
+            'email': '',
+            'password': '',
+            'passwordConfirmation': '',
+            'city': ''}
+    errors = {}
 
-    if not term:
-        for elem in users:
-            out_list.append(elem.get('first_name'))
-        return render_template('users/index.html', names=out_list)
-    else:
-        for elem in users:
-            nol = elem.get('first_name').lower()
-            if nol[0].find(term[0].lower()) == 0:
-            
-                in_list.append(elem.get('first_name'))
+    return render_template(
+        'users/new.html',
+        user=user,
+        errors=errors
+
+    )
 
 
-        out_list  = filter(lambda x: term.lower() in x.lower(), in_list)
 
-        return render_template('users/index.html', names=out_list, first_name=term)
+@app.route('/users/new')
+def users_new():
+    user = {'name': '',
+            'email': '',
+            'password': '',
+            'passwordConfirmation': '',
+            'city': ''}
+    errors = {}
+
+    return render_template(
+        'users/new.html',
+        user=user,
+        errors=errors
+    )
